dc4787f4b1c35174c272ff8174c6c353a0305163
Brian Sims <briansims623@gmail.com>
2023-11-13 18:33:58 -0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # File Metadata Microservice
 
-This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+This is a freeCodeCamp project where a user can upload a file. The file name,type,and size are then returned in a json object.
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var cors = require('cors');
+const mongoose = require('mongoose')
+const multer = require('multer')
+require('express-async-errors')
 require('dotenv').config()
 
 var app = express();
+
+const upload = multer({ dest: './public/data/uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const { originalname: name, mimetype: type, size } = req.file
+  console.log(req.file, req.body)
+  res.json({ name: name, type: type, size: size })
 });
 
 
